40423c590f2b4c88c6241ab5c71fa1aba15fc35d
shakibkhan13 <mds893472@gmail.com>
2024-03-18 22:40:33 +0600

    Shakib_Dev

--- home/models.py
+++ home/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Animal(models.Model):
-    # user = models.ForeignKey(User, on_Delete=models.SET_NULL , null=True ,blank=True)
     Animal_name = models.CharField(max_length=100)
-    Animal_Weight = models.TextField()
+    Animal_Weight = models.CharField(max_length=100)
     Animal_description = models.TextField()
     Animal_image = models.ImageField(upload_to="animal")
-    Animal_Prize = models.TextField()
-    Animal_Farm = models.TextField()
+    Animal_Prize = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
+    Animal_Farm = models.TextField(default='')
     Animal_view_count = models.IntegerField(default=1)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
--- home/views.py
+++ home/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Animal
 
 def animal(request):
-
-
     return render(request, 'Home.html')
 
 def blogs(request):
@@ -22,6 +20,26 @@ def login(request):
     return render(request, 'login.html')
 
 def seller(request):
+    if request.method == "POST":
+        data = request.POST
+        Animal_name = data.get('Animal_name')
+        Animal_Weight = data.get('Animal_Weight')
+        Animal_Prize = data.get('Animal_Prize')
+        Animal_Farm = data.get('Animal_Farm')
+        Animal_description = data.get('Animal_description')
+        Animal_image = request.FILES.get('Animal_image')
+
+        Animal.objects.create(
+            Animal_name=Animal_name,
+            Animal_Weight=Animal_Weight,
+            Animal_Prize=Animal_Prize,
+            Animal_Farm=Animal_Farm,
+            Animal_description=Animal_description,
+            Animal_image=Animal_image
+        )
+    else:
+        return render(request, 'seller.html')
+
     return render(request, 'seller.html')
 
 def cart(request):
