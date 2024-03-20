f4e131ec03785176aeb072cc16104387d83c7b5d
shakibkhan13 <mds893472@gmail.com>
2024-03-20 13:17:16 +0600

    shakib_dev

--- home/models.py
+++ home/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class Animal(models.Model):
     Animal_name = models.CharField(max_length=100)
-    Animal_Weight = models.DecimalField(max_digits=5, decimal_places=2)
-    Animal_Prize = models.DecimalField(max_digits=8, decimal_places=2)
+    Animal_Weight = models.TextField()
+    Animal_Prize = models.TextField()
     Animal_Farm = models.CharField(max_length=100, default='Unknown')
     Animal_description = models.TextField()
-    Animal_image = models.ImageField(upload_to='animal_images', default='https://via.placeholder.com/150')
+    Animal_image = models.ImageField(upload_to='animal_images', default='')
 
     def __str__(self):
         return self.Animal_name
