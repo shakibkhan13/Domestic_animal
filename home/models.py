a549f21cfaccd4407c62c33a484082c6292b665a
shakibkhan13 <mds893472@gmail.com>
2024-03-25 23:52:35 +0600

    shakib_dev

--- home/models.py
+++ home/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db import models
 
 class Animal(models.Model):
     Animal_name = models.CharField(max_length=100)
-    Animal_Weight = models.TextField()
-    Animal_Prize = models.TextField()
+    Animal_Weight = models.DecimalField(max_digits=10, decimal_places=2) 
+    Animal_Prize = models.DecimalField(max_digits=10, decimal_places=2) 
     Animal_Farm = models.CharField(max_length=100, default='Unknown')
+    Animal_color = models.CharField(max_length=100, default='Unknown')
     Animal_description = models.TextField()
     Animal_image = models.ImageField(upload_to='animal_images', default='')
 
